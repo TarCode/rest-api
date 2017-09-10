82404185f8c72c97ced43b908628f1a5406d55e8
TarCode <tarcode33@gmail.com>
2017-09-10 12:53:59 +0200

    imported files setup server file

--- index.js
+++ index.js
@@ -1,28 +1,23 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+
 const app = express()
 
-const MongoClient = require('mongodb').MongoClient
+const port = process.env.PORT || 3000
 
-var url = 'mongodb://localhost:27017/test';
+const mongoose = require('mongoose')
 
-const mongo = () => {
-    const url = process.env.MONGO_URL || 'mongodb://localhost/list-test'
-    return MongoClient.connect(url, (err, database) => {
-        if (err) return console.log('error while connecting to ' + url, err);
-        return database
-    })
-}
+Item = require('./list-model')
 
-const items = db => db.collection('items')
+mongoose.Promise = global.Promise
+mongoose.connect('mongodb://localhost/list-test')
 
+app.use(bodyParser.urlencoded({ extended: true }))
+app.user(bodyParser.json())
 
-app.get('/', function (req, res) {
-    mongo()
-    .then(db => {
-        res.send(items.find())  
-    })
-})
+const routes = require('./list-routes')
+routes(app)
 
-app.listen(3000, function () {
-    console.log('Example app listening on port 3000!')
+app.listen(port, function() {
+    console.log("App listening on port ", port);
 })
