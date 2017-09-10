e89c8dfec47ca9eb93b3b2031a538cd091c3568f
TarCode <tarcode33@gmail.com>
2017-09-10 13:12:17 +0200

    added morgan logs and config for db (for test env)

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const morgan = require('morgan'
+)
+let config = require('config')
 
 const app = express()
 
@@ -9,8 +12,23 @@ const mongoose = require('mongoose')
 
 Item = require('./item-model')
 
+let options = {
+    server: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } },
+    replset: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } },
+    useMongoClient: true
+}; 
+
 mongoose.Promise = global.Promise
-mongoose.connect('mongodb://localhost/list-test', { useMongoClient: true })
+mongoose.connect(config.DBHost, options)
+
+let db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'))
+
+if (config.util.getEnv('NODE_ENV') !== 'test') {
+    app.use(morgan('combined'))
+}
+
+app.get("/", (req, res) => res.json({ message: "Welcome to Items Land!" }));
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
